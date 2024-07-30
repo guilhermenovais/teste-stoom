82c63606b413d466dd68cb1a1acd40db226512b4
Guilherme Novais <guilhermenovaisdesouza@proton.me>
2024-07-29 22:22:29 -0300

    feat: creating category entity

---
+++ src/main/java/br/com/stoom/store/model/Category.java
@@ -0,0 +1,36 @@
+package br.com.stoom.store.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Column;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_sequence")
+    @SequenceGenerator(name = "category_sequence", sequenceName = "CATEGORY_SEQ")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
