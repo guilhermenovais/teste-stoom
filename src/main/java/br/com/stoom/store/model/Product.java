fd419737ec8d910dcf3e6fdf43353c08249d981d
Guilherme Novais <guilhermenovaisdesouza@proton.me>
2024-07-29 22:22:58 -0300

    feat: adding attributes to product entity

--- src/main/java/br/com/stoom/store/model/Product.java
+++ src/main/java/br/com/stoom/store/model/Product.java
@@ -10,6 +10,55 @@ public class Product {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "sku")
-    private String sku;
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    private Category category;
+
+    @ManyToOne
+    private Brand brand;
+
+    @Column(name = "price")
+    private Double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public  Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
 }
