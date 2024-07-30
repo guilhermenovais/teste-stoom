8b2cad74573ec900e654eb971bd4154c05d6ca42
Guilherme Novais <guilhermenovaisdesouza@proton.me>
2024-07-29 22:26:44 -0300

    feat: creating CategoryController

---
+++ src/main/java/br/com/stoom/store/controller/CategoryController.java
@@ -0,0 +1,39 @@
+package br.com.stoom.store.controller;
+
+import br.com.stoom.store.business.CategoryBO;
+import br.com.stoom.store.model.Category;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryBO categoryService;
+
+    @GetMapping(value = "/")
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> c = categoryService.findAll();
+        if(!c.isEmpty())
+            return new ResponseEntity<>(c, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(value = "/")
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        Category newCategory = categoryService.saveCategory(category);
+        return new ResponseEntity<>(newCategory, HttpStatus.OK);
+    }
+}
