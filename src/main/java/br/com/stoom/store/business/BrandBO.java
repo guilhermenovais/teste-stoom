9a75e988e0c0b92edc6f85eb7f646acae59dc8d4
Guilherme Novais <guilhermenovaisdesouza@proton.me>
2024-07-29 22:25:41 -0300

    feat: creating BrandBO

---
+++ src/main/java/br/com/stoom/store/business/BrandBO.java
@@ -0,0 +1,24 @@
+package br.com.stoom.store.business;
+
+import br.com.stoom.store.business.interfaces.IBrandBO;
+import br.com.stoom.store.model.Brand;
+import br.com.stoom.store.repository.BrandRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BrandBO implements IBrandBO {
+
+    @Autowired
+    private BrandRepository brandRepository;
+
+    @Override
+    public List<Brand> findAll() { return brandRepository.findAll(); }
+
+    @Override
+    public Brand saveBrand(Brand brand) {
+        return brandRepository.save(brand);
+    }
+}
---
+++ src/main/java/br/com/stoom/store/business/interfaces/IBrandBO.java
@@ -0,0 +1,11 @@
+package br.com.stoom.store.business.interfaces;
+
+import br.com.stoom.store.model.Brand;
+
+import java.util.List;
+
+public interface IBrandBO {
+    List<Brand> findAll();
+
+    Brand saveBrand(Brand brand);
+}
